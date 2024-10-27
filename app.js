40d1af76f7ace911147e2603e3cde6eca6a13977
Shalvah <diakon.ng@gmail.com>
2024-10-27 19:57:05 +0100

    Chart

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-import {Hono} from "hono";
-import { bearerAuth } from 'hono/bearer-auth'
-import { serve } from '@hono/node-server'
-import { DatabaseSync } from "node:sqlite";
+import {Hono} from "hono"
+import {bearerAuth} from 'hono/bearer-auth'
+import {basicAuth} from 'hono/basic-auth'
+import {serve} from '@hono/node-server'
+import {DatabaseSync} from "node:sqlite"
+
+const token = process.env.MERCURY_AUTH_TOKEN
+const username = process.env.MERCURY_USERNAME
+const password = process.env.MERCURY_PASSWORD
 
 const db = new DatabaseSync('db.sqlite')
 db.exec(`
-CREATE TABLE IF NOT EXISTS readings (
-    value REAL NOT NULL,
-    created_at INTEGER NOT NULL
-) STRICT
+    CREATE TABLE IF NOT EXISTS readings
+    (
+        value      REAL    NOT NULL,
+        created_at INTEGER NOT NULL
+    ) STRICT
 `)
-
-const token = process.env.AUTH_TOKEN;
 
 const app = new Hono();
 
@@ -19,7 +23,7 @@ app.get('/', (c) => c.text("Alright, you found me. Happy?"))
 
 app.post(
   '/readings',
-  bearerAuth({ token }),
+  bearerAuth({token}),
   (c) => {
     const reading = c.req.query('r')
     const ts = c.req.query('ts')
@@ -28,7 +32,52 @@ app.post(
     const result = insert.run(reading, ts)
 
     return c.text(result.changes, 201)
-})
+  })
+
+app.get(
+  '/chart',
+  basicAuth({username, password}),
+  (c) => {
+    const query = db.prepare(`SELECT *
+                              FROM readings`);
+    const readings = query.all();
+
+    const html = `
+      <div><canvas id="myChart"></canvas></div>
+
+      <script src="https://cdn.jsdelivr.net/npm/chart.js@^3"></script>
+      <script src="https://cdn.jsdelivr.net/npm/luxon@^2"></script>
+      <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-luxon@^1"></script>
+      
+      <script>
+        const ctx = document.getElementById('myChart');
+        const chart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: ${JSON.stringify(readings.map(r => r.created_at * 1000))},
+            datasets: [{
+              label: "Temperature",
+              data: ${JSON.stringify(readings.map(r => r.value))},
+              borderColor: 'rgb(75, 192, 192)',
+            }],
+          },
+          options: {
+            scales: {
+              x: {
+                type: 'timeseries',
+                time: {
+                    unit: 'second',
+                }
+              }
+            }
+          }
+        });
+      </script>
+    `;
+
+    return c.html(html);
+  }
+)
 
 serve({
   fetch: app.fetch,
