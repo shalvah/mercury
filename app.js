33f44cb6e54ad3dcd17f65379f6346d2225b5425
Shalvah <diakon.ng@gmail.com>
2024-10-27 18:54:34 +0100

    Auth

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 import {Hono} from "hono";
+import { serve } from '@hono/node-server'
+
+const auth = process.env.AUTH_TOKEN;
 
 const app = new Hono();
 
 app.get('/', (c) => c.text("Alright, you found me. Happy?"))
 
-app.post('/readings', (c) => {
+app.post(
+  '/readings',
+  (c) => {
+    const token = c.req.query('t')
+    if (token !== auth) {
+      return c.text('', 401)
+    }
 
+    const reading = c.req.query('r')
+
+    store(r)
+
+    return c.text('', 201)
 })
 
-export default app;
+serve({
+  fetch: app.fetch,
+  port: 8787,
+});
